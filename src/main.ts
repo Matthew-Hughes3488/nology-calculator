853cf18033bf1bc9d08be182b87c131d33f32791
Matthew <12hughesm@gmail.com>
2023-11-24 11:35:12 +0000

    Removed all code except the fetching of elements

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,15 @@
 import "./main.scss";
 
 let equation: string = "";
-let result: number = 0;
-const digitRegex = new RegExp(/^[0-9]$/);
+let firstNumber: number;
+let secondNumber: number;
+let opperator: string;
+
+const digitRegex = new RegExp(/^[0-9 .]$/);
 const operatorRegex = new RegExp(/[+\-x÷=%C]/);
 
+// FETCHING AND VALIDATING ALL NEEDED ELEMENTS
 const buttons = document.querySelectorAll(".buttons__button");
 if (buttons.length === 0) throw new Error("Error with query all");
-
 const userOutput = document.querySelector(".calculator__output");
 if (!userOutput) throw new Error("Error with query selector");
-
-const resetUserOutput = (resetString = "") =>{
-    userOutput.textContent = resetString;
-}
-
-const addToUserOutput = (update: string) => {
-  userOutput.textContent += update;
-};
-
-const addToEquation = (charToAdd: string) => {
-  equation += charToAdd;
-  console.log(equation);
-  
-};
-
-const clearEquation = () =>{
-    equation = "";
-}
-
-const handleButtonPress = (event: Event) => {
-  const input = event.target as HTMLButtonElement;
-  const buttonInput = input.innerHTML;
-
-  // REPLACE WITH PROPER CODE LATER
-  if (digitRegex.test(buttonInput)) {
-    addToUserOutput(buttonInput);
-    addToEquation(buttonInput);
-  } else if (operatorRegex.test(buttonInput)) {
-    alert("Is a operator");
-  }
-};
-buttons.forEach((button) => {
-  button.addEventListener("click", handleButtonPress);
-});
