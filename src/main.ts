f86d6b7f2ee23603d711995020ae32c6eaad647a
Matthew <12hughesm@gmail.com>
2023-11-24 10:34:11 +0000

    Created a button press handler function and added the event listener to all buttons

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
 import './main.scss'
 
+let mathExpression: string = '';
+let result: number = 0;
+const digitRegex = new RegExp(/[0-9]/);
+const symbolRegeex = new RegExp(/^[+\-x÷]$/);
 
+const buttons = document.querySelectorAll(".buttons__button");
+if(buttons.length === 0) throw new Error("Error with query all");
+
+const handleButtonPress = (event:Event) =>{
+    const input = event.target as HTMLButtonElement;
+    const buttonInput = input.innerHTML;
+
+    // REPLACE WITH PROPER CODE LATER
+    if(digitRegex.test(buttonInput)) alert("Is a number");
+    else if(symbolRegeex.test(buttonInput)) alert("Is a symbol");
+}
+
+buttons.forEach(button =>{
+    button.addEventListener("click", handleButtonPress);
+})
